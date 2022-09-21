5251945905ddc6bc22df3d1f47c2361105a0f5ec
Mr4K5 <113898911+ryantzyryan@users.noreply.github.com>
2022-09-21 13:50:07 +0700

    Add files via upload

---
+++ s2.sh
@@ -0,0 +1,22 @@
+#!/system/bin/sh
+
+echo "######################################################"
+          figlet  Mr4K5
+echo "######################################################"
+
+echo " Welcome To My Tools "
+
+sleep 2
+
+read -p " Target :"
+if
+then
+echo "[+] Nomor :"
+else
+sleep3
+echo "[x] Thank You "
+sleep2
+clear
+bash s2.sh
+fi
+
