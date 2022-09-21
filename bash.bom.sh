57ff5fc7355fa12ae1318e40a5ce22314c0e8472
Mr4K5 <113898911+ryantzyryan@users.noreply.github.com>
2022-09-21 15:44:00 +0700

    Add files via upload

---
+++ bash.bom.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "################################"
+figlet Mr4K5
+echo "################################"
+read -p "Target :" no:
+curl -s https://github.com/ryantzyryan/u0_a318
+
+if [ $n -eq 101 ]: then
+
+echo "[*] Kill" 
+elif "[ $n -q 510 ]"
+echo "[_] DANGER"
+elif "[ $n -q 999 ]" : thend
+echo "[*] USE WISELY"
+else
+echo "[_] DONE"
+fi
