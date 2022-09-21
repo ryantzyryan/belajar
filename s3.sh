9efa207f24a2aa897af7674d962ba8c2e893348f
Mr4K5 <113898911+ryantzyryan@users.noreply.github.com>
2022-09-21 14:02:18 +0700

    Add files via upload

---
+++ s3.sh
@@ -0,0 +1,17 @@
+#!/system/bin/sh
+
+clear
+figlet " Login "
+
+
+read -p "[+] masukan id :" id
+if  [  $ID = " Facebook " ]:
+then
+echo "[√] sedang loads tunggu coneccting........"
+else
+echo "[×] proses selesai crack sukses "
+sleep 2
+back "[99]
+sh s3.sh
+if
+
